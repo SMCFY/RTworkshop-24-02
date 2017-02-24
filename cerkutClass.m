021e85a6be792d372aeb941798a1d80b9f32418d
cerkut <Cumhur.Erkut@gmail.com>
2017-02-24 23:48:12 +0100

    Magnitude boosting of a particular bin while keeping the phase continuous.

--- cerkutClass.m
+++ cerkutClass.m
@@ -1,21 +1,32 @@
-classdef inClass < audioPlugin
+classdef cerkutClass < audioPlugin
     %INCLASS Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        Width = 0.5;
+        Width = 1;
     end
     
     properties (Constant)
       PluginInterface = audioPluginInterface( ...
-            audioPluginParameter('Width', 'Mapping',{'lin',0,1}))
+            audioPluginParameter('Width', 'Mapping',{'lin',0,10}))
     end
     methods
         function out = process(obj,in)
           [m,n] = size(in);
-           X = fft(in,m);
-           X(1:round(m*obj.Width),:) = 0;
-           out = real(ifft(X)); 
+          X = fft(in,m*2);
+          XHalf       = X(1:m+1,:);
+          out = zeros(m,n);
+          
+          for i = 1:n % Channels
+             YmagHalf     = abs(XHalf(:,i));
+             YmagHalf(6)  = YmagHalf(6)*obj.Width;
+             YHalf        = YmagHalf.*exp(1i*angle(XHalf(:,i)));
+             reverseYHalf = flipud( conj(YHalf(2:end-1)) );
+             Y            = [YHalf;reverseYHalf];
+             y            = real(ifft(Y));
+             out(:,i)     = y(1:m);
+          end
+           
         end
     end
     
