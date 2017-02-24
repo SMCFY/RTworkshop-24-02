bba5b3abce256eff2446fdc10f583587f51e7134
MainUser <Luca@h1843.eduroam-5.wlan.aalborguniversitet.dk>
2017-02-24 15:47:34 +0100

    Time Shifting Scratches

---
+++ CasperTimeShifting.m
@@ -0,0 +1,26 @@
+classdef Casper < audioPlugin
+    %INCLASS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Width = 0.5;
+        Shift = 0;
+    end
+    
+    properties (Constant)
+      PluginInterface = audioPluginInterface( ...
+            audioPluginParameter('Shift', 'Mapping',{'lin',0,2}))
+    end
+    
+    methods
+        function out = process(obj,in)
+          [m,n] = size(in);
+           X = fft(in,m);
+           for w = 1:m
+        %       X(w,:)=X(w,:)*(1/(i*w*44100/m))*exp(-1*i*obj.Shift*(w*44100/m));
+           end;
+           out = real(ifft(X)); 
+        end
+    end
+    
+end
