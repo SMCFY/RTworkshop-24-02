8f72301a3a3e75c4cc435380a73f9d23b6737e15
Michael Castanieto <michaelcastanieto@h274.aau1x.klient.sydhavn2.site.aau.dk>
2017-02-24 15:43:37 +0100

    added LFO function

--- MichaelPresident.m
+++ MichaelPresident.m
@@ -3,9 +3,20 @@ classdef MichaelPresident
     %   Detailed explanation goes here
     
     properties
+      modfreq
+      phase = 0;
     end
     
     methods
+      % This is an LFO for the hamming bandpass filter
+      function y = LFO(freq)
+        y = sin(obj.phase);
+        delta = obj.modfreq*2*pi;
+        obj.phase = obj.phase + delta;
+        if obj.phase > 2 * pi
+  	      obj.phase = obj.phase - 2*pi;
+        end
+      end
     end
     
 end
